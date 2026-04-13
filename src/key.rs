a1abba21d1c22683960701d71d1b66b2cb09b43c
xcus33me <HarvieOrigin@yandex.ru>
2026-04-13 18:18:50 +0300

    123

--- src/key.rs
+++ src/key.rs
@@ -6,8 +6,10 @@ pub type UserKey = Bytes;
 // User defined value type
 pub type UserValue = Bytes;
 
+pub type SeqNo = u64;
+
 #[derive(Clone, Copy, Eq, PartialEq)]
-enum ValueType {
+pub enum ValueType {
     /// Value exists
     Value,
 
@@ -27,6 +29,49 @@ impl ValueType {
 #[derive(Clone, PartialEq, Eq)]
 pub struct InternalKey {
     pub user_key: UserKey,
-    pub seqno: u64,
+    pub seqno: SeqNo,
     pub value_type: ValueType,
+}
+
+impl InternalKey {
+    #[must_use]
+    pub fn new<K: Into<UserKey>>(user_key: K, seqno: SeqNo, value_type: ValueType) -> Self {
+        Self {
+            user_key: user_key.into(),
+            seqno: seqno,
+            value_type: value_type,
+        }
+    }
+
+    pub fn is_tombstone(&self) -> bool {
+        self.value_type.is_tombstone()
+    }
+}
+
+impl PartialOrd for InternalKey {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for InternalKey {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.user_key
+            .cmp(&other.user_key)
+            .then(self.seqno.cmp(&other.seqno).reverse())
+    }
+}
+
+pub struct InternalValue {
+    pub key: InternalKey,
+    pub value: UserValue,
+}
+
+impl InternalValue {
+    pub fn new<V: Into<UserValue>>(key: InternalKey, value: V) -> Self {
+        Self {
+            key: key,
+            value: value.into(),
+        }
+    }
 }
--- src/memtable.rs
+++ src/memtable.rs
@@ -2,7 +2,7 @@ use std::sync::atomic::AtomicU64;
 
 use crossbeam_skiplist::SkipMap;
 
-use crate::key::{InternalKey, UserValue};
+use crate::key::{InternalKey, InternalValue, SeqNo, UserKey, UserValue, ValueType};
 
 pub(crate) struct MemTable {
     data: SkipMap<InternalKey, UserValue>,
@@ -12,10 +12,39 @@ pub(crate) struct MemTable {
 }
 
 impl MemTable {
+    #[must_use]
     pub fn new() -> Self {
         Self {
             data: SkipMap::default(),
             size_bytes: AtomicU64::default(),
         }
+    }
+
+    // Returns the item by key (with the height seqno)
+    pub fn get(&self, key: &UserKey, seqno: SeqNo) -> Option<InternalValue> {
+        if seqno == 0 {
+            return None;
+        }
+
+        let lower_bound = InternalKey {
+            user_key: key.clone(),
+            seqno: seqno - 1,
+            value_type: ValueType::Value,
+        };
+
+        let entry = self.data.range(lower_bound..).next()?;
+
+        if &entry.key().user_key != key {
+            return None;
+        }
+
+        if entry.key().value_type.is_tombstone() {
+            return None;
+        }
+
+        Some(InternalValue::new(
+            entry.key().clone(),
+            entry.value().clone(),
+        ))
     }
 }
